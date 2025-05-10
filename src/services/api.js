30b18e7e2af52c34ce075f071dc2bb5277bd7403
Gabriel Matte Elias <gabrielmatteelias@gmail.com>
2025-05-10 18:03:18 -0300

    bkd link mockado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'https://validawhats-bkd.onrender.com/v1';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
